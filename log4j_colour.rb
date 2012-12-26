1ab69f1886bdfe56b5f2b829bf458227f6757847
Michael Freeman <mikfreeman@gmail.com>
2012-12-26 11:37:26 +0000

    Adding Script

---
+++ log4j_colour.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+red = "\e[31m" 
+green = "\e[32m"
+yellow = "\e[33m"
+cyan = "\e[36m"
+
+currentColour = red
+
+ARGF.each do |line|
+
+ if line.include?('INFO') 
+  currentColour = green
+ elsif line.include?('TRACE') 
+  currentColour = cyan
+ elsif line.include?('DEBUG') 
+  currentColour = cyan
+ elsif line.include?('ERROR') 
+  currentColour = red
+ elsif line.include?('FATAL') 
+  currentColour = red
+ elsif line.include?('WARN') 
+  currentColour = yellow
+ end
+  puts "#{currentColour}#{line}"
+end
